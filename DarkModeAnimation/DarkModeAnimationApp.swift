b9b6e1d060978c93be9cdde21273eff8fda43514
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-06 16:20:34 +0300

    Initial Commit

---
+++ DarkModeAnimation/DarkModeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeAnimationApp.swift
+//  DarkModeAnimation
+//
+//  Created by Yunus Emre Taşdemir on 6.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
